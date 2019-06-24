29c5e0d111bc4e4d548759b908ad4f8626cc5d2d
Hans Ole Hatzel <hhatzel@gmail.com>
2019-06-24 15:36:45 +0200

    Add setup.py to build egg for spark submssion

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "reddit_import",
+    version = "0.0.1",
+    author = "Hans Ole Hatzel",
+    author_email = "hhatzel@gmail.com",
+    description = (""),
+    license = "COMMERCIAL",
+    keywords = "",
+    packages=['reddit_import'],
+    classifiers=[],
+)
