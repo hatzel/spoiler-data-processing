6fbaeedcb7e518920308a4645ce8096f98b14045
Hans Ole Hatzel <hhatzel@gmail.com>
2019-07-16 13:10:30 +0200

    Extend results processing to support multiple annotaiton schemes

--- results-processor.py
+++ results-processor.py
@@ -2,28 +2,43 @@
 import argparse
 import markdown
 import pyspark
+from pyspark.sql.functions import rand
 from reddit_import import util
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 SPLITS = [
     ("train", 0.7),
-    ("dev1", 0.1),
-    ("dev2", 0.1),
-    ("test", 0.1),
+    ("dev", 0.1),
+    ("test", 0.2),
 ]
 
 
 def build_parser():
     parser = argparse.ArgumentParser(description="Result Transformation")
-    parser.add_argument("--text-mode", nargs="?", choices=["html", "plain", "raw"],
-                        help="Output mode of text", default="raw")
-    # parser.add_argument("label-scale", nargs=1, choices=["document", "token"],
-    #                     help="Class labels on token or document level.", default="document")
+    parser.add_argument("--text-mode", choices=["html", "plain"],
+                        help="Output mode of text", default="plain")
+    parser.add_argument("--classify", choices=["document", "token"],
+                        help="Class labels on token or document level.", default="document")
     parser.add_argument(
         "--comments", nargs="+",
         help="Text files to convert, usually one spoiler and one non spoiler file.",
     )
     return parser
+
+
+def convert_text(text, text_mode, classify, renderer):
+    remove_other_tags = text_mode == "plain"
+    remove_spoiler_tags = classify == "document"
+    tag_whitelist = [] if remove_spoiler_tags else ["spoiler"]
+    tag_blacklist = ["spoiler"] if remove_spoiler_tags else []
+    return remove_tags(
+        convert_to_spoiler_tags(
+            renderer.convert(text)
+        ),
+        remove_others=remove_other_tags,
+        whitelist=tag_whitelist,
+        blacklist=tag_blacklist,
+    )
 
 
 def main(args):
@@ -38,24 +53,38 @@ def main(args):
         extensions=["spoilers"]
     )
     parsed_comments = comments.select("text", "contains_spoiler")\
+        .orderBy(rand())\
         .rdd\
         .map(lambda row: (
-            parse_text(row["text"], renderer),
+            convert_text(row["text"], args.text_mode, args.classify, renderer),
             row["contains_spoiler"],
         ))
     output = session.createDataFrame(parsed_comments, ["text", "spoiler"]).cache()
     split_data = output.randomSplit([value for _, value in SPLITS], seed=1)
     for i, (split, _) in enumerate(SPLITS):
         split_data[i].write.json(
-            "reddit/%s-%s-%s.json"
-            % (split, args.text_mode, session.sparkContext.applicationId)
+            "reddit/%s-%s-%s-%s-%s.json"
+            % (split, args.text_mode, args.text_mode, args.classify, session.sparkContext.applicationId)
         )
 
 
-def parse_text(text, renderer):
-    rendered = renderer.convert(text)
-    soup = BeautifulSoup(rendered, "html.parser")
-    return soup.get_text()
+def convert_to_spoiler_tags(html):
+    """Converts span tags with spoiler class to spoiler tag."""
+    soup = BeautifulSoup(html)
+    for tag in soup.find_all(True):
+        if tag.name == "span" and "spoiler" in tag.get("class"):
+            tag.name = "spoiler"
+            del tag["class"]
+    return soup
+
+
+def remove_tags(soup, remove_others, whitelist=tuple(), blacklist=tuple()):
+    """Remove all but whitelisted tags."""
+    for tag in soup.find_all(True):
+        if (remove_others and tag.name not in whitelist) or (tag.name in blacklist):
+            tag.unwrap()
+    return str(soup)
+
 
 
 if __name__ == "__main__":
