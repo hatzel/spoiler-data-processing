d2d4cb70733c5c53bc45274fd931bee9851adbf3
Hans Ole Hatzel <hhatzel@gmail.com>
2019-10-09 14:27:11 +0200

    Fix minHash based deduplication

--- reddit_import/dedup.py
+++ reddit_import/dedup.py
@@ -25,7 +25,10 @@ def dedup_min_hash(df, column, id_col, min_distance=0.1):
     joined = dedup_model.approxSimilarityJoin(with_token_ids, with_token_ids, 1 - min_distance, distCol="dist")\
         .drop("token_ids", "hashes")\
         .filter(f"datasetA.{id_col} < datasetB.{id_col}")
-    duplicate_ids = joined.rdd.flatMap(lambda row: ([row.datasetA[id_col]], [row.datasetB[id_col]])).toDF()
+    duplicate_ids = joined.rdd.flatMap(lambda row: (row.datasetA[id_col], row.datasetB[id_col]))\
+        .distinct()\
+        .map(lambda el: [el])\
+        .toDF()
     return df.join(duplicate_ids, duplicate_ids._1 == df[id_col], "left")\
         .where(duplicate_ids._1.isNotNull())\
         .drop(duplicate_ids._1)
